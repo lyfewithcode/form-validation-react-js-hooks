5ac43e1caed984e04e30930f35b59b50e255b8a7
lyfewithcode <lyfewithcode@gmail.com>
2021-08-10 01:22:11 +0700

    Creating Validation Code

--- src/FormSignUp.js
+++ src/FormSignUp.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import useForm from './useForm'
+import validateInfo from './validateInfo'
 
 const FormSignUp = () => {
-    const { handleChange, values, handleSubmit } = useForm()
+    const { handleChange, values, handleSubmit } = useForm(validate)
 
     return (
         <div className='form-content-right'>
